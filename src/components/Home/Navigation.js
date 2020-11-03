119a342ed8809ec504f0c4334d34525d82bb2af2
Quingo <lbarylak@pm.me>
2020-11-03 14:44:30 +0100

    added second section, also public directory

--- src/components/Home/Navigation.js
+++ src/components/Home/Navigation.js
@@ -1,22 +1,65 @@
 import React from "react";
-import {Link} from 'react-scroll';
+import { Link } from "react-scroll";
 import "../../scss/navigation/navigation.scss";
 
-
-const Navigation = () =>{
-    return(
-        <nav className="nav">
-            <div>
-            </div>
-            <ul className="nav__list">
-                <Link className="nav__list__item" activeClass="active" to="firstSection" smooth={true} offset={0} duration={500}>Start</Link>
-                <Link className="nav__list__item" activeClass="active" to="secondSection" smooth={true} offset={0} duration={500}>O co chodzi?</Link>
-                <Link className="nav__list__item" activeClass="active" to="about" smooth={true} offset={0} duration={500}>O nas</Link>
-                <Link className="nav__list__item" activeClass="active" to="whoHelp" smooth={true} offset={20} duration={500}>Fundacja i organizacja</Link>
-                <Link className="nav__list__item" activeClass="active" to="footer" smooth={true} offset={0} duration={500}>Kontakt</Link>
-            </ul>
-        </nav>
-    )
-}
+const Navigation = () => {
+  return (
+    <nav className="nav">
+      <div></div>
+      <ul className="nav__list">
+        <Link
+          className="nav__list__item"
+          activeClass="active"
+          to="firstSection"
+          smooth={true}
+          offset={0}
+          duration={500}
+        >
+          Start
+        </Link>
+        <Link
+          className="nav__list__item"
+          activeClass="active"
+          to="secondSection"
+          smooth={true}
+          offset={0}
+          duration={500}
+        >
+          O co chodzi?
+        </Link>
+        <Link
+          className="nav__list__item"
+          activeClass="active"
+          to="about"
+          smooth={true}
+          offset={0}
+          duration={500}
+        >
+          O nas
+        </Link>
+        <Link
+          className="nav__list__item"
+          activeClass="active"
+          to="whoHelp"
+          smooth={true}
+          offset={20}
+          duration={500}
+        >
+          Fundacja i organizacje
+        </Link>
+        <Link
+          className="nav__list__item"
+          activeClass="active"
+          to="footer"
+          smooth={true}
+          offset={0}
+          duration={500}
+        >
+          Kontakt
+        </Link>
+      </ul>
+    </nav>
+  );
+};
 
 export default Navigation;
--- src/components/Home/SecondSection.js
+++ src/components/Home/SecondSection.js
@@ -1,6 +1,44 @@
 import React from "react";
+import "./../../scss/home/secondSection.scss";
+
+import decoration from "../../assets/Decoration.svg";
+import icon1 from "../../assets/Icon-1.svg";
+import icon2 from "../../assets/Icon-2.svg";
+import icon3 from "../../assets/Icon-3.svg";
+import icon4 from "../../assets/Icon-4.svg";
 
 const SecondSection = () => {
-    return null;
-}
+  return (
+    <section className="secondSection" id="secondSection">
+      <h1 className="secondSection__title">Wystarczą 4 proste kroki</h1>
+      <img src={decoration} alt="decoration" className="secondSection__img" />
+
+      <div className="secondSection__content">
+        <div className="secondSection__content__box">
+          <img src={icon1} alt="icon" />
+          <h2 className="secondSection__content__box__title">Wybierz rzeczy</h2>
+          <p>ubrania, zabawki, sprzęt i inne</p>
+        </div>
+        <div className="secondSection__content__box">
+          <img src={icon2} alt="icon" />
+          <h2 className="secondSection__content__box__title">Spakuj je</h2>
+          <p>skorzystaj z worków na śmieci</p>
+        </div>
+        <div className="secondSection__content__box">
+          <img src={icon3} alt="icon" />
+          <h2 className="secondSection__content__box__title">
+            Zdecyduj komu chcesz pomóc
+          </h2>
+          <p>wybierz zaufane miejsce</p>
+        </div>
+        <div className="secondSection__content__box">
+          <img src={icon4} alt="icon" />
+          <h2 className="secondSection__content__box__title">Zamów kuriera</h2>
+          <p>kurier przyjedzie w dogodnym terminie</p>
+        </div>
+      </div>
+    </section>
+  );
+};
+
 export default SecondSection;
